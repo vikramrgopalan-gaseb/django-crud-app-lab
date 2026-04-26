fa7fd3285af3a543884aec5519dcd523e7ede23b
vikramgopalan-gaseb <vikram.r.gopalan@gmail.com>
2026-04-26 17:12:18 -0600

    4/26 v1

--- sneakercollector/models.py
+++ sneakercollector/models.py
@@ -8,23 +8,22 @@ CONDITION = (
 )
 
 # Create your models here.
-class Cat(models.Model):
-    name = models.CharField(max_length=100)
-    breed = models.CharField(max_length=100)
-    description = models.TextField(max_length=250)
-    age = models.IntegerField()
+class Sneaker(models.Model):
+    band = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    colorway = models.TextField(max_length=250)
+    releasedate = models.IntegerField()
 
     def __str__(self):
         return self.name
     
     def get_absolute_url(self):
-        return reverse('cat-detail', kwargs={'cat_id': self.id})
+        return reverse('sneaker-detail', kwargs={'sneaker_id': self.id})
     
-class Feeding(models.Model):
-    date = models.DateField("Feeding Date")
-    meal = models.CharField(max_length=1, choices=MEALS, default=MEALS[0][0])
+class Condition(models.Model):
+    condition = models.CharField(max_length=1, choices=CONDITION, default=CONDITION[0][0])
 
-    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+    sneaker = models.ForeignKey(Sneaker, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.get_meal_display()} on {self.date}"
