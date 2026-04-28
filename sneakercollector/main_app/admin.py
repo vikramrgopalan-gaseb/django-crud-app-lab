3433e8165bf4fc282914ed9c8ddf64851ff42c0a
vikramgopalan-gaseb <vikram.r.gopalan@gmail.com>
2026-04-28 16:59:02 -0600

    4/28 v3

--- sneakercollector/main_app/admin.py
+++ sneakercollector/main_app/admin.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
-from .models import Sneaker, Condition
+from .models import Sneaker, Collection, Condition
 
 # Register your models here.
 admin.site.register(Sneaker)
+admin.site.register(Collection)
 admin.site.register(Condition)
-
-from django.contrib import admin
-from .models import Cat, Feeding, Toy
-
-# Register your models here.
-admin.site.register(Cat)
-admin.site.register(Feeding)
-admin.site.register(Toy)
