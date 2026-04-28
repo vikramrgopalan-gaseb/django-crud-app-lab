89d7ee14032c4a68ba4d7d27d33dae62d98c94a6
vikramgopalan-gaseb <vikram.r.gopalan@gmail.com>
2026-04-28 16:27:07 -0600

    4/28 v1

--- sneakercollector/views.py
+++ sneakercollector/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Sneaker
+from django.views.generic import ListView, DetailView
+from .models import Sneaker, Condition
+from .forms import CollectingForm
 # Create your views here.
-
-
 
 def home(request):
     return render(request, 'home.html')
@@ -13,83 +13,58 @@ def about(request):
     return render(request, 'about.html')
 
 def sneaker_index(request):
-    return render(request, 'sneakers/index.html', {'sneakers': sneakers})
+    sneaker = Sneaker.objects.all()
+    return render(request, 'sneakers/index.html', {'sneakes': sneakers})
 
 def sneaker_detail(request, sneaker_id):
     sneaker = Sneaker.objects.get(id=sneaker_id)
-    return render(request, 'sneakers/detail.html', {'sneaker': sneaker})
+    condition_sneaker_doesnt_have = Condition.objects.exclude(id__in = sneaker.condition.all().values_list('id'))
+    collecting_form = CollectingForm()
+    return render(request, 'sneakers/detail.html', {'sneakers': sneaker, 'collecting_form': collecting_form, 'condition': condition_sneaker_doesnt_have})
 
 class SneakerCreate(CreateView):
     model = Sneaker
-    fields = '__all__'
-    # success_url = '/sneakers/'
+    fields = ['brand', 'model', 'colorway', 'year']
+    # success_url = '/cats/'
 
 class SneakerUpdate(UpdateView):
     model = Sneaker
-    fields = ['brand', 'model', 'colorway', 'releasedate']
+    fields = ['brand', 'model', 'colorway', 'year']
 
 class SneakerDelete(DeleteView):
     model = Sneaker
     success_url = '/sneakers/'
 
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic import ListView, DetailView
-from .models import Cat, Toy
-from .forms import FeedingForm
-# Create your views here.
-
-def home(request):
-    return render(request, 'home.html')
-
-def about(request):
-    return render(request, 'about.html')
-
-def cat_index(request):
-    cats = Cat.objects.all()
-    return render(request, 'cats/index.html', {'cats': cats})
-
-def cat_detail(request, cat_id):
-    cat = Cat.objects.get(id=cat_id)
-    feeding_form = FeedingForm()
-    return render(request, 'cats/detail.html', {'cat': cat, 'feeding_form': feeding_form})
-
-class CatCreate(CreateView):
-    model = Cat
-    fields = '__all__'
-    # success_url = '/cats/'
-
-class CatUpdate(UpdateView):
-    model = Cat
-    fields = ['breed', 'description', 'age']
-
-class CatDelete(DeleteView):
-    model = Cat
-    success_url = '/cats/'
-
-def add_feeding(request, cat_id):
-    form = FeedingForm(request.POST)
+def add_collecting(request, sneaker_id):
+    form = CollectingForm(request.POST)
     if form.is_valid():
-        new_feeding = form.save(commit=False)
-        new_feeding.cat_id = cat_id
-        new_feeding.save()
-    return redirect('cat-detail', cat_id=cat_id)
+        new_collecting = form.save(commit=False)
+        new_collecting.sneaker_id = sneaker_id
+        new_collecting.save()
+    return redirect('sneaker-detail', sneaker_id=sneaker_id)
 
-class ToyCreate(CreateView):
-    model = Toy
-    fields = '__all__'
+class ConditionCreate(CreateView):
+    model = Condition
+    fields = ['DS', 'NDS', 'W']
 
-class ToyList(ListView):
-    model = Toy
+class ConditionList(ListView):
+    model = Condition
 
-class ToyDetail(DetailView):
-    model = Toy
+class ConditionDetail(DetailView):
+    model = Condition
 
-class ToyUpdate(UpdateView):
-    model = Toy
-    fields = ['name', 'color']
+class ConditionUpdate(UpdateView):
+    model = Condition
+    fields = ['DS', 'NDS', 'W']
 
-class ToyDelete(DeleteView):
-    model = Toy
-    success_url = '/toys/'
+class ConditionDelete(DeleteView):
+    model = Condition
+    success_url = '/sneakers/'
+
+def associate_condition(request, sneaker_id, condition_id):
+    Sneaker.objects.get(id=sneaker_id).condition.add(condition_id)
+    return redirect('sneaker-detail', sneaker_id=sneaker_id)
+
+def remove_condition(request, sneaker_id, condition_id):
+    Sneaker.objects.get(id=sneaker_id).condition.remove(condition_id)
+    return redirect('sneaker-detail', sneaker_id=sneaker_id)
